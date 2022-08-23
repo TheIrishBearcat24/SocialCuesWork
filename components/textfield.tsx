bcb5ef6a787345709127a77807250f168cb5250e
TheIrishBearcat24 <clarkeck@mail.uc.edu>
2022-08-22 21:38:16 -0400

    Beginning attempt at state reactive textbox

---
+++ components/textfield.tsx
@@ -0,0 +1,12 @@
+import { TextField, Autocomplete } from "@mui/material";
+import React, { useState } from "react";
+
+const socialCuesList: string[] = []
+
+const Textbox = () => {
+    const [getSocialCuesList, setSocialCuesList] = useState(socialCuesList)
+}
+
+const AutocompleteSearchBox: React.FC = () => {
+    return null
+}
