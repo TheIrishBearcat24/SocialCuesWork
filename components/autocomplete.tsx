a94979c42cb17ad6a5478872fcd6d64566860f73
TheIrishBearcat24 <clarkeck@mail.uc.edu>
2022-08-26 13:03:30 -0400

    Added more code, but still buggy

--- components/autocomplete.tsx
+++ components/autocomplete.tsx
@@ -6,13 +6,19 @@ import { Autocomplete,
     DialogContentText,
     DialogActions,
     TextField,
-    createFilterOptions } from "@mui/material";
+    createFilterOptions, 
+    Dialog} from "@mui/material";
 
 interface IAutocompleText {
+    inputValue?: string
     text: string
 }
 
 const textFilter = createFilterOptions<IAutocompleText>();
+
+const socialCuesArray: IAutocompleText[] = [
+  {text: "How to behave in a work setting"}
+]
 
 export default function createAddableAutocompleteBox() {
     const [getValue, setValue] = useState<IAutocompleText | null>(null)
@@ -38,5 +44,94 @@ export default function createAddableAutocompleteBox() {
         handleClose();
       };
 
-      return null
+      return (
+        <Fragment>
+          <Autocomplete>
+            value={value}
+            onChange={(event, newValue: any) => {
+              if (typeof newValue == "string") {
+                setTimeout(() => {
+                  toggleOpen(true);
+                  setDialogValue({
+                    text: newValue
+                  });
+                });
+              }
+
+              else if (newValue && newValue.inputValue) {
+                toggleOpen(true);
+                setDialogValue({
+                  text: newValue
+                });
+              }
+
+              else {
+                setValue(newValue)
+              }
+            }}
+
+            filterOtions={(options, params) => {
+              const filtered = textFilter(options, params)
+
+              if (params.inputValue != "") {
+                filtered.push({
+                  inputValue: params.inputValue,
+                  text: `Add "${params.inputValue}"`
+                })
+              }
+
+              return filtered;
+            }}
+
+            id="autocomplete-social-cues-textfield"
+            options={socialCuesArray}
+            getOptionLabel={(option) => {
+              if (typeof option == "string") {
+                return option;
+              }
+
+              if (option.inputValue) {
+                return option.inputValue;
+              }
+
+              return option.text;
+            }}
+            selectOnFocus
+            clearOnBlur
+            handleHomeEndKeys
+            renderOption={(props: any, option: any) => <li {...props}>{option.text}</li>}
+            freeSolo
+            renderInput={(params: any) => <TextField {...params} label="Social Cues Dialog" />}
+          </Autocomplete>
+          <Dialog open={open} onClose={handleClose}>
+            <form onSubmit={handleSubmit}>
+              <DialogTitle>Add a new Social Cue</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Feel free to add your own social cue here!
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  value={getDialogValue.text}
+                  onChange={(event) =>
+                    setDialogValue({
+                      ...getDialogValue,
+                      text: event.target.value,
+                    })
+                  }
+                  label="socialCuesText"
+                  type="text"
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type="submit">Add</Button>
+              </DialogActions>
+            </form>
+          </Dialog>
+        </Fragment>
+      );
 }
