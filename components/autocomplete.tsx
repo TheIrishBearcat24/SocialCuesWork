571eaa581457aa4303b6c6afaec43b7e14425083
TheIrishBearcat24 <clarkeck@mail.uc.edu>
2022-08-25 21:47:56 -0400

    WIP

--- components/autocomplete.tsx
+++ components/autocomplete.tsx
@@ -1,4 +1,42 @@
-import React, { useState } from "react";
-import { v4 as uuid } from 'uuid';
-import { Autocomplete } from "@mui/material";
+import React, { useState, Fragment } from "react";
+import { Autocomplete,
+    Button,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    TextField,
+    createFilterOptions } from "@mui/material";
 
+interface IAutocompleText {
+    text: string
+}
+
+const textFilter = createFilterOptions<IAutocompleText>();
+
+export default function createAddableAutocompleteBox() {
+    const [getValue, setValue] = useState<IAutocompleText | null>(null)
+    const [open, toggleOpen] = useState(false)
+
+    const handleClose = () => {
+        setDialogValue({
+          text: ""
+        });
+        toggleOpen(false);
+      };
+
+      const [getDialogValue, setDialogValue] = useState({
+        text: ""
+      })
+
+      const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setValue({
+            text: getDialogValue.text
+        });
+
+        handleClose();
+      };
+
+      return null
+}
